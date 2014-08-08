56bdb200bcf4af1bc0f759535bd60d41cb2ea022
vivacityinfotech.jaipur <vivacityinfotech.jaipur@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-08 12:06:29 +0000

    Added a new version number
    git-svn-id: https://plugins.svn.wordpress.org/facebook-comment-by-vivacity/tags/1.0.5@962378 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fb-comments.php
+++ fb-comments.php
@@ -3,7 +3,7 @@
 * Plugin Name: Facebook Comments by Vivacity
 * Plugin URI: http://www.vivacityinfotech.net
 * Description: A simple Facebook Comments plugin for your blog that enables FB User`s to comment on your blog or website.
-* Version: 1.0.4
+* Version: 1.0.5
 * Author: Vivacity Infotech Pvt. Ltd.
 * Author URI: http://www.vivacityinfotech.net
 Text Domain: facebook-comment-by-vivacity
